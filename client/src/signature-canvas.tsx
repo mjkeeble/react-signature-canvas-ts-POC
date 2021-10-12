20846eb5b3bf49a53b7b2e1fd766dc5d6e09aea2
Mark Keeble <mjkeeble@yahoo.de>
2021-10-12 14:51:32 +0200

    minor tidy up, renamed handlers

--- client/src/signature-canvas.tsx
+++ client/src/signature-canvas.tsx
@@ -1,71 +1,66 @@
-import React, {useState, useRef} from 'react';
-import ReactSignatureCanvas from 'react-signature-canvas';
-import SignatureCanvas from 'react-signature-canvas';
-
+import React, { useState, useRef } from "react";
+import SignatureCanvas from "react-signature-canvas";
 
 export default function SigCanvas() {
-    const [exportedSignatureURL,setExportedSignatureURL] = useState(null);
-    const [signatureData, setSignatureData] = useState();
-    let sigPad = useRef<ReactSignatureCanvas>();
-    
-    function clear () {
-        clearSignatureData();
-        sigPad.clear();
-    };
+	const [exportedSignatureURL, setExportedSignatureURL] = useState("");
+	const [signatureData, setSignatureData] = useState([]);
 
-    function clearSignatureData() {
-        setSignatureData([]);
-        console.log(`data cleared`)
-    }
+	// *** sigPad needs to be properly typed ***
+	let sigPad = useRef<any>();
+	function handleClearCanvas() {
+		clearSignatureData();
+		sigPad.current?.clear();
+	}
+	function clearSignatureData() {
+		setSignatureData([]);
+	}
+	function handlePrevious() {
+		const currentRender = sigPad.current?.toData();
+		if (signatureData && !signatureData.length) {
+			setSignatureData(currentRender);
+		}
+		sigPad.current?.fromData(currentRender.slice(0, currentRender.length - 1));
+	}
+	function handleNext() {
+		const currentRenderPosition = sigPad.current.toData().length;
+		if (!signatureData.length) return;
+		sigPad.current.fromData(signatureData.slice(0, currentRenderPosition + 1));
+	}
+	function handleExportSignature() {
+		setSignatureData(sigPad.current?.toData());
+		setExportedSignatureURL(sigPad.current?.toDataURL("svg"));
+		handleClearCanvas();
+	}
+	function handleShowData() {
+		console.clear();
+		console.log(`current signature`);
+		console.log(sigPad.current?.toData());
 
-    function previous () {
-        const currentRender = sigPad.toData()
-        if (!signatureData.length) {
-            setSignatureData(currentRender);
-        };
-        sigPad.fromData(currentRender.slice(0 , currentRender.length - 1))
-    };
-
-    function next () {
-        const currentRenderPosition = sigPad.toData().length
-        if (!signatureData.length) return;
-        sigPad.fromData(signatureData.slice(0,currentRenderPosition + 1))
-        
-    };
-
-    function exportSignature () {
-        setSignatureData(sigPad.toData());
-        setExportedSignatureURL ( sigPad.toDataURL('svg') );
-        clear();
-    };
-
-    function showData () {
-        console.clear()
-        console.log(`current signature`)
-        console.log(sigPad.toData())
-        console.log({signatureData})
-    }
-    
-    return (
-        <div>
-            <div>
-                <SignatureCanvas ref={ (ref) => {sigPad = ref}}
-                backgroundColor='rgba(153,204, 255,1)'
-                penColor='rgba(0,0,153,1)'
-                canvasProps={{width: 500, height: 200, className: 'sigCanvas'}} 
-                onEnd={clearSignatureData} />
-            </div>
-            
-            <div>
-                <button onClick={previous}>Previous</button>
-                <button onClick={clear}>Clear</button>
-                <button onClick={next}>Next</button>
-                <button onClick={exportSignature}>Export</button>
-                <button onClick={showData}>Console.log(data)</button>
-            </div>
-            {exportedSignatureURL
-            ? <img src={exportedSignatureURL} alt="Your signature" />
-            : null}
-        </div>
-    )
+	}
+	return (
+		<div>
+			<div>
+				<SignatureCanvas
+					ref={(ref) => {
+						sigPad.current = ref;
+					}}
+					backgroundColor="rgba(153,204, 255,1)"
+					penColor="rgba(0,0,153,1)"
+					canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+					onEnd={clearSignatureData}
+				/>
+			</div>
+			​
+			<div>
+				<button onClick={handlePrevious}>Previous</button>
+				<button onClick={handleClearCanvas}>Clear</button>
+				<button onClick={handleNext}>Next</button>
+				<button onClick={handleExportSignature}>Export</button>
+				<button onClick={handleShowData}>Show signature data (console)</button>
+			</div>
+			{exportedSignatureURL ? (
+				<img src={exportedSignatureURL} alt="Your signature" />
+			) : null}
+		</div>
+	);
 }
